e5279a02c3ec9501961f0d9873ce0697724072f5
Chun-Min Chang <chun.m.chang@gmail.com>
2016-03-27 00:03:36 +0800

    add async, defer in loader.js

--- js/loader.js
+++ js/loader.js
@@ -10,10 +10,23 @@ Loader.prototype = {
   _callback: null,
   _count: 0,
   _debug: false,
+  _async: false,
+  // _defer: false,
 
-  require: function(scripts, callback) {
+  require: function(scripts, callback, async/*, defer*/) {
     this._scripts = scripts;
-    this._callback = callback;
+
+    if (callback) {
+      this._callback = callback;
+    }
+
+    if (async) {
+      this._async = async;
+    }
+
+    // if (defer) {
+    //   this._defer = defer;
+    // }
 
     for (let i = 0 ; i < this._scripts.length ; ++i) {
       let script = this._scripts[i];
@@ -25,7 +38,8 @@ Loader.prototype = {
     this._log('_writeScript: ' + source);
     let script = document.createElement('script');
     script.type = 'text/javascript';
-    script.async = true;
+    script.async = this._async;
+    // script.defer = this._defer;
     script.src = source;
     script.addEventListener('load', this._loaded.bind(this), false);
     document.head.appendChild(script);
